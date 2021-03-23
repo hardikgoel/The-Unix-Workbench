cde4d8505d5b84d0f8b8ddc73e0272602f304414
hardikgoel <40266172+hardikgoel@users.noreply.github.com>
2021-03-23 18:43:13 +0530

    COUNTING ONLY FILES AND NOT DIRECTORIES

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 function guess_file_count {
-    answer=$(ls | wc -l)
+    answer=$(ls -Ap | wc -l)
+    guess = -1
     while [[ $guess != $answer ]]
     do
-        echo "$guessed"
         echo "how many files are in the current directory?: "
         read guess
         if [[ $guess -gt $answer ]]
